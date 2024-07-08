ebe404db66875dabc608cea2eb3ca69846e97a63
Matanda Phiri <matandaphiri64@gmail.com>
2024-07-08 11:11:56 -0500

    connect to Mongodb

--- src/app.js
+++ src/app.js
@@ -1,23 +1,56 @@
 //Load packages required for program
-const mongodb = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
 
-//Set up MongoDB
-const MongoClient = mongodb.MongoClient;
+// //Set up MongoDB
+// const MongoClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'Bulk Uploader';
+// const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'Bulk Uploader data';
 
-MongoClient.connect(connectionURL, {}, (error, client) => {
-    if(error){
-        return console.log('Unable to connect to Database')
+// MongoClient.connect(connectionURL, {}, (error, client) => {
+//     if(error){
+//         return console.log('Unable to connect to Database')
+//     }
+
+//     console.log('Goood!');
+// })
+
+
+
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://127.0.0.1:27017";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
     }
+);
 
-    console.log('Goood!');
-})
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db(databaseName);
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 //start express
 const app = express();
