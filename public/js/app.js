3672596e1e781dae85ff59da6d5d4824f89ad681
Matanda Phiri <matandaphiri64@gmail.com>
2024-07-15 09:26:20 -0500

    Update styles.css

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,24 @@
 'use strict'
 
+const collectionName = document.getElementById('collection-name');
+
+const collectionNameInit = () => {
+    collectionName.value = "collection name";
+    collectionName.classList.add('placeholder-text');
+}
+
+//initialize collection name input field
+collectionNameInit();
+
+collectionName.addEventListener('blur', () => {
+    if(collectionName.value === ""){
+        collectionNameInit();
+    }
+})
+
+collectionName.addEventListener('focusin', () => {
+    if(collectionName.value === "collection name"){
+        collectionName.value = "";
+    }
+    collectionName.classList.remove('placeholder-text');
+})
